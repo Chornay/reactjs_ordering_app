b80b62261381b030f8a0cbd6bab0c4dac598d884
Muhammad Hijazi (MBA) <irekasoft@gmail.com>
2018-12-28 20:48:42 +0800

    small update, added react-burger-menu to be implemented

--- src/screens/NotificationsMainScreen.js
+++ src/screens/NotificationsMainScreen.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class NotificationMainScreen extends Component {
    render() {
        return (
-           <div>
+           <div className="container">
                <h1>NotificationMainScreen</h1>
            </div>
        );
